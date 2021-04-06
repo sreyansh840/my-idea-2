159d9d97f2ba65133ed860b73d3d209f41a06cde
sreyansh840 <74823699+sreyansh840@users.noreply.github.com>
2021-04-06 19:16:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,31 @@
 var gameState="welcome";
 var welcomePic ;
-var welcome,arrow;
+var welcome,arrow,arrowPic;
+var groupStory,groupStage_1,groupStage_2,groupStart;
 
 
 function preload(){
   welcomePic=loadImage("loading.gif");
+  arrowPic=loadImage("arroow.png");
 }
 
 
 function setup() {
   createCanvas(displayWidth-20,displayHeight-200);
   welcome=createSprite(650,300,50,50);
-
+  arrow=createSprite(700,600,50,50);
+  welcome.addImage(welcomePic);
+  arrow.addImage(arrowPic);
 }
 
 function draw() {
   background(220);
   drawSprites();
+
   
   if(gameState==="welcome"){
-  welcome.addImage(welcomePic);
-}
+    text("Press to start",690,600);
+   
+    }
 
 }
