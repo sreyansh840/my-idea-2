f3eb8834a8acd7c901cf813f71aa6fefe694410d
sreyansh840 <74823699+sreyansh840@users.noreply.github.com>
2021-04-14 14:26:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,79 @@
-var gameState="welcome";
-var welcomePic ;
-var welcome,arrow,arrowPic;
-var groupStory,groupStage_1,groupStage_2,groupStart;
+var gameState;
+var welcomePic,arrowPic ;
+var welcomeSpr,arrowSpr,storySpr;
+var groupStory,groupStage_1,groupStage_2,groupWelcome;
+var moveSpr;
 
 
 function preload(){
-  welcomePic=loadImage("loading.gif");
-  arrowPic=loadImage("arroow.png");
+  welcomePic=loadImage("sprites/loading.gif");
+  arrowPic=loadImage("sprites/arrow.png");
 }
 
 
 function setup() {
+  //create the canvas
   createCanvas(displayWidth-20,displayHeight-200);
-  welcome=createSprite(650,300,50,50);
-  arrow=createSprite(700,600,50,50);
-  welcome.addImage(welcomePic);
-  arrow.addImage(arrowPic);
+
+  //initial value of game state
+  gameState="welcome";
+
+  //create all the sprites required in your project
+  welcomeSpr=createSprite(650,300,50,50);
+  arrowSpr=createSprite(700,525,50,50);
+  moveSpr=createSprite(600,400,20,20);
+  welcomeSpr.addImage(welcomePic);
+  arrowSpr.addImage(arrowPic);
+  arrowSpr.scale=0.35;
+
+  storySpr=createSprite(20,20,40,40);
+
+  arrowSpr.debug=true;
+  //create the group
+  groupWelcome=new Group();
+  groupStory=new Group();
+  groupStage_1=new Group();
+  groupStage_2=new Group();
+
+  //tell the sprites in Welcome group
+  groupWelcome.add(welcomeSpr);
+  groupWelcome.add(arrowSpr);
+
+  groupStory.add(storySpr);
+
+  groupStory.add(moveSpr);
+
 }
 
 function draw() {
-  background(220);
+  background("yellow");
   drawSprites();
-
+  text(gameState,200,40);
   
   if(gameState==="welcome"){
-    text("Press to start",690,600);
+      text("Press to start",690,600);
+      groupWelcome.setVisibleEach(true);
+      storySpr.visible=false;
+      groupStory.setVisibleEach(false);
+      groupStage_1.setVisibleEach(false);
+      groupStage_2.setVisibleEach(false);
+      
+      if(mousePressedOver(arrowSpr)){
+        gameState="story";
+      }
+      
    
-    }
+  }
+  if(gameState==="story"){
+    groupWelcome.setVisibleEach(false);
+      groupStory.setVisibleEach(true);
+      groupStage_1.setVisibleEach(false);
+      groupStage_2.setVisibleEach(false);
+      text("____ is a news reporter ",200,50);
+
+      if(mousePressedOver(moveSpr)){
+        gameState="stage_1";
+      }
+  }
 
 }
